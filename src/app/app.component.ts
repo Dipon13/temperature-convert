18ad2c1de047e8d16fe942f83e76e684606d1bda
Dipon13 <mukherjeedipon.13@gmail.com>
2022-06-04 16:46:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'temperature_convert';
+  //declare variables here
+  result:number=0
+  final:any
+  title = 'Temperature Converter';
+  convert(temp : number,type : string){
+     console.log(typeof(temp)); //delete later
+     console.log(temp); //delete later
+     console.log(typeof(type)); //delete later
+     console.log(type); //delete later
+     if (type=="f") {
+       this.result=((temp-32)*5)/9
+       this.final=(this.result.toFixed(2)+" °C")
+       console.log(this.final)
+      } else {
+        this.result=temp*(9/5)+32
+        // console.log(this.result.toFixed(2)+" F")
+        this.final=(this.result.toFixed(2)+" °F")
+        console.log(this.final)
+     }
+  }
+
 }
